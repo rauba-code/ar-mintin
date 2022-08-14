e5ceca1a8e660a22c2903b396bca3deb7052cfe5
rauba-code <arnoldas.rauba@ktu.edu>
2022-08-14 18:19:20 +0300

    Refactored simulation to component-based design

--- src/sim.rs
+++ src/sim.rs
@@ -48,32 +48,20 @@ pub enum UiMessageLegacy<'a> {
     NotifyAssessment,
 }
 
-pub enum UiMessage<'a> {
-    Assess(&'a TableEntry),
-    Display(&'a TableEntry),
+#[derive(Clone, Debug)]
+pub enum TMessage<T> {
+    Assess(T),
+    Display(T),
     NotifyAssessment,
 }
+
+pub type UiMessage<'a> = TMessage<&'a TableEntry>;
 
 pub struct Simulation<'a> {
     pub pt: ProgressTable<'a>,
     pub args: SimArgs,
-    state: State,
-    last_msg: Option<UiMessage<'a>>,
-    v1: Vec<(usize, &'a TableEntry)>,
-    v2: Vec<(usize, &'a TableEntry)>,
-    last_entry: Option<(usize, &'a TableEntry)>,
-}
-
-enum State {
-    Begin,
-    Assessment,
-    Learning(LearningState),
-    NotifyAssessment,
-}
-
-enum LearningState {
-    ShowEntry,
-    Assess,
+    last_msg: Option<TMessage<Tie<'a>>>,
+    state: Main<'a>,
 }
 
 impl<'a> Simulation<'a> {
@@ -81,11 +69,8 @@ impl<'a> Simulation<'a> {
         Simulation {
             pt,
             args,
-            state: State::Begin,
             last_msg: None,
-            last_entry: None,
-            v1: Vec::new(),
-            v2: Vec::new(),
+            state: Main::new(),
         }
     }
 
@@ -123,107 +108,36 @@ impl<'a> Simulation<'a> {
         rpass
     }
 
-    fn next_inner(&mut self, post: &Option<String>) -> UiMessage {
-        const LEARN_SESSIONS: usize = 10;
-        const ASSESS_SESSIONS: usize = 10;
-        let mut rng = thread_rng();
-
-        loop {
-            let (msg_opt, nstate): (Option<UiMessage>, State) = match &mut self.state {
-                State::Begin => {
-                    self.v1 = self
-                        .pt
-                        .select_random_entries(ASSESS_SESSIONS, true, || rng.gen::<f64>());
-                    (None, State::Assessment)
-                }
-                State::Assessment => match &mut self.v1.pop() {
-                    None => {
-                        self.v1 = self
-                            .pt
-                            .select_random_entries(LEARN_SESSIONS, false, || 0_f64);
-                        (None, State::Learning(LearningState::ShowEntry))
-                    }
-                    Some(tail) => {
-                        self.last_entry = Some(*tail);
-                        (Some(UiMessage::Assess(tail.1)), State::Assessment)
-                    }
-                },
-                State::Learning(LearningState::ShowEntry) => match &mut self.v1.pop() {
-                    None => (None, State::NotifyAssessment),
-                    Some(entry) => {
-                        if !self.args.classic {
-                            self.v2 = vec![*entry]
-                        } else {
-                            self.v2.clear()
-                        }
-                        (
-                            Some(UiMessage::Display(entry.1)),
-                            State::Learning(LearningState::Assess),
-                        )
-                    }
-                },
-                State::Learning(LearningState::Assess) => {
-                    match self.last_msg {
-                        Some(UiMessage::Display(_)) => {
-                            self.v2.extend(
-                                self.pt
-                                    .select_random_entries(1, true, || rng.gen::<f64>())
-                                    .iter(),
-                            );
-                        }
-                        Some(UiMessage::Assess(_)) => {
-                            let tpost = post.clone().unwrap();
-                            if !self.last_entry.unwrap().1.assess(tpost.clone()) {
-                                self.v2.extend(
-                                    self.pt
-                                        .select_random_entries(1, true, || {
-                                            thread_rng().gen::<f64>()
-                                        })
-                                        .iter(),
-                                );
-                                if !self.args.classic {
-                                    self.v2.push(self.last_entry.unwrap());
-                                }
-                            }
-                        }
-                        _ => {
-                            panic!();
-                        }
-                    }
-                    match &mut self.v2.pop() {
-                        None => (None, State::NotifyAssessment),
-                        Some(tail) => {
-                            self.last_entry = Some(*tail);
-                            (
-                                Some(UiMessage::Assess(tail.1)),
-                                State::Learning(LearningState::Assess),
-                            )
-                        }
-                    }
-                }
-                State::NotifyAssessment => {
-                    self.v1 = self
-                        .pt
-                        .select_random_entries(ASSESS_SESSIONS, true, || rng.gen::<f64>());
-                    (Some(UiMessage::NotifyAssessment), State::Assessment)
-                }
-            };
-            self.state = nstate;
-            if let Some(msg) = msg_opt {
-                return msg;
-            }
-        }
-    }
-
     pub fn next(&mut self, post: Option<String>) -> UiMessage {
         assert_eq!(
-            matches!(self.last_msg, Some(UiMessage::Assess(_))),
+            matches!(self.last_msg, Some(TMessage::Assess(_))),
             post.is_some()
         );
 
-        self.next_inner(&post)
+        let pass = if let Some(TMessage::Assess(ent)) = self.last_msg {
+            let b = ent.1.assess(post.unwrap());
+            self.ptset(ent.0, b);
+            b
+        } else {
+            false
+        };
+        let inp = &mut Input {
+            pt: &mut self.pt,
+            args: &mut self.args,
+        };
+
+        let r = self.state.next(inp, pass, 1);
+        eprintln!();
+        eprintln!();
+        self.last_msg = r.clone();
+        match r.unwrap() {
+            TMessage::Assess(a) => UiMessage::Assess(a.1),
+            TMessage::Display(a) => UiMessage::Display(a.1),
+            TMessage::NotifyAssessment => UiMessage::NotifyAssessment,
+        }
     }
 
+    #[warn(deprecated)]
     pub fn simulate<F>(&mut self, uimsg: &F)
     where
         F: Fn(UiMessageLegacy),
@@ -278,5 +192,224 @@ impl<'a> Simulation<'a> {
             // NotifyAssessment
             uimsg(UiMessageLegacy::NotifyAssessment);
         }
+    }
+}
+
+pub struct Input<'a, 'b> {
+    pt: &'b mut ProgressTable<'a>,
+    args: &'b mut SimArgs,
+}
+
+const MAXDEPTH: u16 = 30;
+pub trait Domain<'a> {
+    fn next<'b>(
+        &mut self,
+        inp: &mut Input<'a, 'b>,
+        pass: bool,
+        depth: u16,
+    ) -> Option<TMessage<Tie<'a>>>
+    where
+        'a: 'b;
+}
+
+#[derive(Debug)]
+pub enum Bivariant<T, U> {
+    V1(T),
+    V2(U),
+}
+
+#[derive(Debug, Default)]
+pub struct Main<'a> {
+    inner: Option<Bivariant<Assessment<'a>, Learning<'a>>>,
+}
+
+impl<'a> Main<'a> {
+    pub fn new() -> Self {
+        Self { inner: None }
+    }
+}
+
+impl<'a> Domain<'a> for Main<'a> {
+    fn next<'b>(
+        &mut self,
+        inp: &mut Input<'a, 'b>,
+        pass: bool,
+        depth: u16,
+    ) -> Option<TMessage<Tie<'a>>>
+    where
+        'a: 'b,
+    {
+        assert!(depth < MAXDEPTH);
+        let indent = unsafe { String::from_utf8_unchecked(vec![b'|'; depth as usize]) };
+        eprintln!("{} {:?}", indent, self);
+        let r = match &mut self.inner {
+            None => {
+                self.inner = Some(Bivariant::V1(Assessment::new(inp)));
+                self.next(inp, pass, depth + 1)
+            }
+            Some(Bivariant::V1(a)) => match a.next(inp, pass, depth + 1) {
+                None => {
+                    self.inner = Some(Bivariant::V2(Learning::new(inp)));
+                    self.next(inp, pass, depth + 1)
+                }
+                Some(b) => Some(b),
+            },
+            Some(Bivariant::V2(a)) => match a.next(inp, pass, depth + 1) {
+                None => {
+                    self.inner = Some(Bivariant::V1(Assessment::new(inp)));
+                    self.next(inp, pass, depth + 1)
+                }
+                Some(b) => Some(b),
+            },
+        };
+        eprintln!("{} {:?}", indent, r);
+        r
+    }
+}
+
+type Tie<'a> = (usize, &'a TableEntry);
+
+#[derive(Debug)]
+pub struct Assessment<'a> {
+    began: bool,
+    ents: Vec<Tie<'a>>,
+}
+
+impl<'a> Assessment<'a> {
+    pub fn new<'b>(inp: &mut Input<'a, 'b>) -> Self {
+        const ASSESS_SESSIONS: usize = 10;
+        let mut rng = thread_rng();
+        let ents = inp
+            .pt
+            .select_random_entries(ASSESS_SESSIONS, true, || rng.gen::<f64>());
+        Self { ents, began: false }
+    }
+}
+
+impl<'a> Domain<'a> for Assessment<'a> {
+    fn next<'b>(
+        &mut self,
+        _: &mut Input<'a, 'b>,
+        _pass: bool,
+        depth: u16,
+    ) -> Option<TMessage<Tie<'a>>>
+    where
+        'a: 'b,
+    {
+        assert!(depth < MAXDEPTH);
+        let indent = unsafe { String::from_utf8_unchecked(vec![b'|'; depth as usize]) };
+        eprintln!("{} {:?}", indent, self);
+        let r = {
+            if self.began {
+                self.ents.pop().map(TMessage::Assess)
+            } else {
+                self.began = true;
+                if self.ents.is_empty() {
+                    None
+                } else {
+                    Some(TMessage::NotifyAssessment)
+                }
+            }
+        };
+        eprintln!("{} {:?}", indent, r);
+        r
+    }
+}
+
+#[derive(Debug)]
+pub struct Learning<'a> {
+    ents: Vec<Tie<'a>>,
+    inner: Option<LearnSingle<'a>>,
+}
+
+impl<'a> Learning<'a> {
+    pub fn new<'b>(inp: &mut Input<'a, 'b>) -> Self {
+        const LEARN_SESSIONS: usize = 10;
+        let mut ents = inp
+            .pt
+            .select_random_entries(LEARN_SESSIONS, false, || 0_f64);
+        ents.reverse();
+        Self { ents, inner: None }
+    }
+}
+
+impl<'a> Domain<'a> for Learning<'a> {
+    fn next<'b>(
+        &mut self,
+        inp: &mut Input<'a, 'b>,
+        _pass: bool,
+        depth: u16,
+    ) -> Option<TMessage<Tie<'a>>>
+    where
+        'a: 'b,
+    {
+        assert!(depth < MAXDEPTH);
+        let indent = unsafe { String::from_utf8_unchecked(vec![b'|'; depth as usize]) };
+        eprintln!("{} {:?}", indent, self);
+        let r = match &mut self.inner {
+            None => self.ents.pop().and_then(|tail| {
+                self.inner = Some(LearnSingle::new(tail));
+                self.next(inp, _pass, depth + 1)
+            }),
+            Some(ls) => ls.next(inp, _pass, depth + 1).or_else(|| {
+                self.inner = None;
+                self.next(inp, _pass, depth + 1)
+            }),
+        };
+        eprintln!("{} {:?}", indent, r);
+        r
+    }
+}
+
+#[derive(Debug)]
+pub struct LearnSingle<'a> {
+    began: bool,
+    head: Option<Tie<'a>>,
+    stack: Vec<Tie<'a>>,
+}
+
+impl<'a> LearnSingle<'a> {
+    pub fn new(head: Tie<'a>) -> Self {
+        Self {
+            began: false,
+            stack: Vec::new(),
+            head: Some(head),
+        }
+    }
+}
+
+impl<'a> Domain<'a> for LearnSingle<'a> {
+    fn next<'b>(
+        &mut self,
+        inp: &mut Input<'a, 'b>,
+        pass: bool,
+        depth: u16,
+    ) -> Option<TMessage<Tie<'a>>>
+    where
+        'a: 'b,
+    {
+        assert!(depth < MAXDEPTH);
+        let indent = unsafe { String::from_utf8_unchecked(vec![b'|'; depth as usize]) };
+        eprintln!("{} {:?}", indent, self);
+        let r = if let Some(vhead) = self.head.filter(|_| !pass || !self.began) {
+            self.began = true;
+            self.head = None;
+            if !inp.args.classic {
+                self.stack.push(vhead);
+            }
+            self.stack.extend(
+                inp.pt
+                    .select_random_entries(1, true, || thread_rng().gen::<f64>())
+                    .iter(),
+            );
+            Some(TMessage::Display(vhead))
+        } else {
+            self.stack.pop().map(|tail| {
+                self.head = Some(tail);
+                TMessage::Assess(tail)
+            })
+        };
+        eprintln!("{} {:?}", indent, r);
+        r
     }
 }
